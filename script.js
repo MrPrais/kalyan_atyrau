274044e18640c77618d9a0c26d18df0ef2917e64
MrPrais <islamnagi56@gmail.com>
2025-06-18 22:08:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-
 function toggleSection(id) {
-  const section = document.getElementById(id);
-  if (section.classList.contains("hidden")) {
-    section.classList.remove("hidden");
-  } else {
-    section.classList.add("hidden");
-  }
+  const el = document.getElementById(id);
+  el.classList.toggle("hidden");
 }
-
 function toggleLanguage() {
-  alert("Языковая поддержка скоро будет обновлена.");
+  alert("Смена языка скоро появится!");
 }
-
 function openAbout() {
   document.getElementById("aboutModal").style.display = "block";
 }
-
 function closeAbout() {
   document.getElementById("aboutModal").style.display = "none";
 }
