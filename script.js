cdf3819f50a8de84ebe890b15b614fbd3b59c318
MrPrais <islamnagi56@gmail.com>
2025-06-18 23:40:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function toggleTab(tabId) {
+  const tabs = document.querySelectorAll('.tab-content');
+  tabs.forEach(tab => tab.classList.remove('active'));
+  const selectedTab = document.getElementById(tabId);
+  if (selectedTab) selectedTab.classList.add('active');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  toggleTab('menu');
+});
