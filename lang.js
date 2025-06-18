1501acd318e22dfc17d7f72a6e9c2755822f8e06
MrPrais <islamnagi56@gmail.com>
2025-06-18 22:15:19 +0300

    Delete lang.js

--- lang.js
+++
@@ -1,9 +0,0 @@
-let currentLang = 'ru';
-
-document.querySelector('a[href="#lang"]').addEventListener('click', () => {
-  currentLang = currentLang === 'ru' ? 'kz' : 'ru';
-
-  document.querySelectorAll('[data-ru]').forEach(el => {
-    el.textContent = el.dataset[currentLang];
-  });
-});
